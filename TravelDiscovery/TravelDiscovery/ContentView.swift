b73375a6df5c011db75902121447b48695397b3f
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-30 20:05:54 +0200

    add popular destinations view

--- TravelDiscovery/TravelDiscovery/ContentView.swift
+++ TravelDiscovery/TravelDiscovery/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
+//MARK: - DiscoverCategoryModel
 struct DiscoverCategoryModel: Identifiable {
     var id = UUID().uuidString
     var name: String
@@ -71,6 +71,21 @@ struct DiscoverCategoriesView: View {
     }
 }
 
+//MARK: - Popular Destinations Model
+struct PoplularDestinationsModel: Identifiable {
+    var id = UUID().uuidString
+    var imageName: String
+    var name: String
+    var country: String
+    
+    static let allDestinations: [PoplularDestinationsModel] = [
+        .init(imageName: "Paris", name: "Paris", country: "France"),
+        .init(imageName: "Place", name: "Tokyo", country: "Japan"),
+        .init(imageName: "City", name: "New York", country: "US"),
+        .init(imageName: "Paris", name: "Paris", country: "France"),
+    ]
+}
+
 //MARK: - Popular Destinations View
 struct PopularDestinationsView: View {
     var body: some View {
@@ -87,13 +102,30 @@ struct PopularDestinationsView: View {
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 8) {
-                    ForEach(0..<5, id: \.self) { num in
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(.gray)
-                            .frame(width: 125, height: 150)
-                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2.0)
-                            .padding(.bottom)
+                    ForEach(PoplularDestinationsModel.allDestinations) { destination in
+                        VStack(alignment: .leading) {
+                            Image(destination.imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 130, height: 130)
+                                .cornerRadius(5)
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 6)
                             
+                            
+                            Text(destination.name)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 8)
+                            
+                            Text(destination.country)
+                                .font(.system(size: 12, weight: .semibold))
+                                .foregroundColor(.gray)
+                                .padding(.horizontal, 8)
+                                .padding(.bottom, 8)
+                        }
+                        .background(Color(white: 0.9))
+                        .shadow(color: .gray, radius: 4, x: 0.0, y: 2.0)
+                        .padding(.bottom)
                     }
                 }
                 .padding(.horizontal)
@@ -125,7 +157,6 @@ struct PopularRestaurantsView: View {
                             .frame(width: 180, height: 65)
                             .shadow(color: .gray, radius: 4, x: 0.0, y: 2.0)
                             .padding(.bottom)
-                            
                     }
                 }
                 .padding(.horizontal)
@@ -156,7 +187,6 @@ struct TrendingCategoriesView: View {
                             .foregroundColor(.gray)
                             .shadow(color: .gray, radius: 4, x: 0, y: 2)
                             .padding(.bottom)
-                            
                     }
                 }
                 .padding(.horizontal)
