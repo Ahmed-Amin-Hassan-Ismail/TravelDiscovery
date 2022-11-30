132fcf539e3361435557bbde3743b1a7d0067b9c
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-30 16:52:36 +0200

    initial commit with adjust xcode project with compitable 12 and support iOS 14.0

---
+++ TravelDiscovery/TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Ahmed Amin on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
